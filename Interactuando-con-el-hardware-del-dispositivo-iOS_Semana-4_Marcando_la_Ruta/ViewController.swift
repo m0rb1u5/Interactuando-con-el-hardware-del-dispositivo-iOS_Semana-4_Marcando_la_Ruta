04417b6c8c002401e9f9d1b7649438cec42d647d
m0rb1u5 <goesrex@gmail.com>
2017-06-02 06:13:11 -0500

    Implementado todos los requerimientos menos obtener una forma de cambiar el tipo de mapa.

--- Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-4_Marcando_la_Ruta/ViewController.swift
+++ Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-4_Marcando_la_Ruta/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var mapa: MKMapView!
+    
+    private let manejador = CLLocationManager()
+    private var penultimo: CLLocation? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.manejador.delegate = self
+        self.manejador.desiredAccuracy = kCLLocationAccuracyBest
+        self.manejador.distanceFilter = 50
+        self.manejador.requestWhenInUseAuthorization()
+        let span = MKCoordinateSpanMake(0.001, 0.001)
+        let region = MKCoordinateRegionMake(self.mapa.centerCoordinate , span)
+        self.mapa.setRegion(region, animated: true)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            self.manejador.startUpdatingLocation()
+            self.mapa.showsUserLocation = true
+        }
+        else {
+            self.manejador.stopUpdatingLocation()
+            self.mapa.showsUserLocation = false
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pin = MKPointAnnotation()
+        pin.title = "(\(String(describing: (manager.location?.coordinate.longitude)!)), \(String(describing: (manager.location?.coordinate.latitude)!)))"
+        pin.subtitle = ""
+        if self.penultimo == nil {
+            self.penultimo = manager.location
+        }
+        else {
+            pin.subtitle = "\(String(describing: (manager.location?.distance(from: self.penultimo!))!)) m"
+            self.penultimo = manager.location
+        }
+        pin.coordinate = (manager.location?.coordinate)!
+        self.mapa.setCenter((manager.location?.coordinate)!, animated: true)
+        self.mapa.addAnnotation(pin)
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let nserror = error as NSError
+        let title = NSLocalizedString("Error \(nserror.code)", comment: "")
+        let message = NSLocalizedString(nserror.localizedDescription, comment: "")
+        let cancelButtonTitle = NSLocalizedString("OK", comment: "")
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { action in
+            NSLog("La alerta acaba de ocurrir.")
+        }
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
 
 }
 
